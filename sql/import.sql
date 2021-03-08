5edf4e27db77182b5e9da76361e229c18386d03f
David Almaraz <david.almaraz@a2odev.com>
2021-03-08 10:06:24 -0400

    Init

---
+++ sql/import.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS tienda;
+CREATE DATABASE tienda CHARSET utf8mb4;
+USE tienda;
+CREATE TABLE fabricante (
+codigo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL
+);
+CREATE TABLE producto (
+codigo INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+precio DOUBLE NOT NULL,
+codigo_fabricante INT UNSIGNED NOT NULL,
+FOREIGN KEY (codigo_fabricante) REFERENCES fabricante(codigo)
+);
